8b7e5baa5113d7e5b6bf6684dfad0ce8aa6b8b07
GotoRen <r0719en@pluslab.org>
2021-01-18 18:35:02 +0900

    🐛 smarty/templates/member_top.tpl のUIを変更

--- php_libs/smarty/templates/member_top.tpl
+++ php_libs/smarty/templates/member_top.tpl
@@ -1,47 +1,75 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<title>{$title}</title>
+    <title>{$title}</title>
 </head>
 <body>
-<div style="text-align:center;">
-<hr>
-<strong>{$title}</strong>
-<hr>
-    <table>
-      <tr>
-        <td style="vertical-align:top;">
-          [ <a href="{$SCRIPT_NAME}?type=logout">ログアウト</a> ]
-	        <br>
-	        <br>
-	        {$disp_login_state}
-        </td>
-        <td style="vertical-align:top;">
-          <div style="text-align: left; margin-left:15px;">
-            <!-- 情報がある場合に通知枠を作成 -->
-            <strong>{$message}</strong>
-            {if ($body)}              
-              <div style="border: dashed 1px; padding: 10px;">
-                <div style="color:red; font-size:small; font-weight: bold;">お知らせ</div>
-                <div style="font-size: small; font-weight: bold;">{$reg_date|date_format:"%Y年%m月%d日"}&nbsp;{$subject|escape:"html"}</div>
-                <div style="font-size: small;">{$body|escape:"html"}</div>
-              </div>
-            {/if}
-            <br>
-            <br>
-            {$last_name|escape:"html"} {$first_name|escape:"html"} さん、こんにちは！
-            <br>
-            <br>
-	          <a href="{$SCRIPT_NAME}?type=modify&action=form">会員登録情報の修正</a>
-            <br>
-            <br>
-	          <a href="{$SCRIPT_NAME}?type=delete&action=confirm">退会する</a>
-            <br>
-          </div>
-        </td>
-      </tr>
-    </table>
-</div>
-{if ($debug_str)}<pre>{$debug_str}</pre>{/if}
+    <div style="text-align:center;">
+        <hr>
+        <strong>{$title}</strong>
+        <hr>
+    </div>
+    <div style="text-align: left; margin-left: 5%;">
+        <table>
+            <tr>
+                <td style="vertical-align: top;">
+                    <!-- status -->
+                    {$disp_login_state}
+                    <!------------->
+                    <br>
+                    <br>
+                    <!-- userinfo -->
+                    {$last_name|escape:"html"} {$first_name|escape:"html"} さん、こんにちは！
+                    <br>
+                    <br>
+                    <!--------------->
+                </td>
+                <td style="vertical-align: top;">
+                    <div style="text-align: left; margin-left: 15px; padding-left: 50px; padding-right: 50px;">
+                        <!-- message -->
+                        <strong>{$message}</strong>
+                        {if ($body)}
+                            <div style="border: dashed 1px; padding: 10px;">
+                                <div style="color:red; font-size:small; font-weight: bold;">お知らせ</div>
+                                <div style="font-size: small; font-weight: bold;">
+                                    {$reg_date|date_format:"%Y年%m月%d日"}&nbsp;{$subject|escape:"html"}</div>
+                                <div style="font-size: small;">{$body|escape:"html"}</div>
+                            </div>
+                        {/if}
+                        <!------------->
+                    </div>
+                </td>
+                <td style="vertical-align:top;">
+                    <div>
+                        <!-- logout -->
+                        [ <a href="{$SCRIPT_NAME}?type=logout">ログアウト</a> ]
+                        <!------------>
+                        <br>
+                        <br>
+                        <!-- fix-userinfo -->
+                        <a href="{$SCRIPT_NAME}?type=modify&action=form">会員登録情報の修正</a>
+                        <!------------------>
+                        <br>
+                        <br>
+                        <!-- delete-user -->
+                        <a href="{$SCRIPT_NAME}?type=delete&action=confirm">退会する</a>
+                        <!----------------->
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+        <hr>
+        <h1>Hello</h1>
+        <hr>
+    
+
+
+
+
+    {if ($debug_str)}
+    <pre>{$debug_str}</pre>{/if}
 </body>
+
 </html>
--- php_libs/smarty/templates_c/52302ef8ad7821758b2e39be83a68841ea314c24_0.file.member_top.tpl.php
+++ php_libs/smarty/templates_c/52302ef8ad7821758b2e39be83a68841ea314c24_0.file.member_top.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2021-01-18 06:46:45
+/* Smarty version 3.1.30, created on 2021-01-18 18:31:42
   from "/Applications/XAMPP/xamppfiles/htdocs/Bulletin-Board/php_libs/smarty/templates/member_top.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_6004b0457a6f11_24875104',
+  'unifunc' => 'content_6005557e44bf69_92026194',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '52302ef8ad7821758b2e39be83a68841ea314c24' => 
     array (
       0 => '/Applications/XAMPP/xamppfiles/htdocs/Bulletin-Board/php_libs/smarty/templates/member_top.tpl',
-      1 => 1599513803,
+      1 => 1610962300,
       2 => 'file',
     ),
   ),
@@ -20,68 +20,95 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6004b0457a6f11_24875104 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6005557e44bf69_92026194 (Smarty_Internal_Template $_smarty_tpl) {
 if (!is_callable('smarty_modifier_date_format')) require_once '/Applications/XAMPP/xamppfiles/htdocs/Bulletin-Board/php_libs/smarty/libs/plugins/modifier.date_format.php';
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+    <title><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
 </title>
 </head>
 <body>
-<div style="text-align:center;">
-<hr>
-<strong><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+    <div style="text-align:center;">
+        <hr>
+        <strong><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
 </strong>
-<hr>
-    <table>
-      <tr>
-        <td style="vertical-align:top;">
-          [ <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
-?type=logout">ログアウト</a> ]
-	        <br>
-	        <br>
-	        <?php echo $_smarty_tpl->tpl_vars['disp_login_state']->value;?>
+        <hr>
+    </div>
+    <div style="text-align: left; margin-left: 5%;">
+        <table>
+            <tr>
+                <td style="vertical-align: top;">
+                    <!-- status -->
+                    <?php echo $_smarty_tpl->tpl_vars['disp_login_state']->value;?>
 
-        </td>
-        <td style="vertical-align:top;">
-          <div style="text-align: left; margin-left:15px;">
-            <!-- 情報がある場合に通知枠を作成 -->
-            <strong><?php echo $_smarty_tpl->tpl_vars['message']->value;?>
-</strong>
-            <?php if (($_smarty_tpl->tpl_vars['body']->value)) {?>              
-              <div style="border: dashed 1px; padding: 10px;">
-                <div style="color:red; font-size:small; font-weight: bold;">お知らせ</div>
-                <div style="font-size: small; font-weight: bold;"><?php echo smarty_modifier_date_format($_smarty_tpl->tpl_vars['reg_date']->value,"%Y年%m月%d日");?>
-&nbsp;<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['subject']->value, ENT_QUOTES, 'UTF-8', true);?>
-</div>
-                <div style="font-size: small;"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['body']->value, ENT_QUOTES, 'UTF-8', true);?>
-</div>
-              </div>
-            <?php }?>
-            <br>
-            <br>
-            <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['last_name']->value, ENT_QUOTES, 'UTF-8', true);?>
+                    <!------------->
+                    <br>
+                    <br>
+                    <!-- userinfo -->
+                    <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['last_name']->value, ENT_QUOTES, 'UTF-8', true);?>
  <?php echo htmlspecialchars($_smarty_tpl->tpl_vars['first_name']->value, ENT_QUOTES, 'UTF-8', true);?>
  さん、こんにちは！
-            <br>
-            <br>
-	          <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
-?type=modify&action=form">会員登録情報の修正</a>
-            <br>
-            <br>
-	          <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
-?type=delete&action=confirm">退会する</a>
-            <br>
-          </div>
-        </td>
-      </tr>
-    </table>
+                    <br>
+                    <br>
+                    <!--------------->
+                </td>
+                <td style="vertical-align: top;">
+                    <div style="text-align: left; margin-left: 15px; padding-left: 50px; padding-right: 50px;">
+                        <!-- message -->
+                        <strong><?php echo $_smarty_tpl->tpl_vars['message']->value;?>
+</strong>
+                        <?php if (($_smarty_tpl->tpl_vars['body']->value)) {?>
+                            <div style="border: dashed 1px; padding: 10px;">
+                                <div style="color:red; font-size:small; font-weight: bold;">お知らせ</div>
+                                <div style="font-size: small; font-weight: bold;">
+                                    <?php echo smarty_modifier_date_format($_smarty_tpl->tpl_vars['reg_date']->value,"%Y年%m月%d日");?>
+&nbsp;<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['subject']->value, ENT_QUOTES, 'UTF-8', true);?>
 </div>
-<?php if (($_smarty_tpl->tpl_vars['debug_str']->value)) {?><pre><?php echo $_smarty_tpl->tpl_vars['debug_str']->value;?>
+                                <div style="font-size: small;"><?php echo htmlspecialchars($_smarty_tpl->tpl_vars['body']->value, ENT_QUOTES, 'UTF-8', true);?>
+</div>
+                            </div>
+                        <?php }?>
+                        <!------------->
+                    </div>
+                </td>
+                <td style="vertical-align:top;">
+                    <div>
+                        <!-- logout -->
+                        [ <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
+?type=logout">ログアウト</a> ]
+                        <!------------>
+                        <br>
+                        <br>
+                        <!-- fix-userinfo -->
+                        <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
+?type=modify&action=form">会員登録情報の修正</a>
+                        <!------------------>
+                        <br>
+                        <br>
+                        <!-- delete-user -->
+                        <a href="<?php echo $_smarty_tpl->tpl_vars['SCRIPT_NAME']->value;?>
+?type=delete&action=confirm">退会する</a>
+                        <!----------------->
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+        <hr>
+        <h1>Hello</h1>
+        <hr>
+    
+
+
+
+
+    <?php if (($_smarty_tpl->tpl_vars['debug_str']->value)) {?>
+    <pre><?php echo $_smarty_tpl->tpl_vars['debug_str']->value;?>
 </pre><?php }?>
 </body>
-</html>
-<?php }
+
+</html><?php }
 }
